d1874a4b2b7d2b8ac43df870b73f3a66155d92e7
Hachim Youssoufa <feihachim@yahoo.fr>
2022-04-14 03:04:03 +0200

    Inscription fonctionnelle kinda

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Cette adresse mail est déjà utilisée!')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -47,7 +47,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function __construct()
     {
         $this->setCreatedAt(new \DateTime('now'));
-        $this->setVerified(false);
+        $this->setVerified(true);
         $this->setPartnership(false);
     }
 
